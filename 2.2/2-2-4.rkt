a4205d04e091579285d1f34694fc8ac02f82a18d
Andrei Dimitrascu <adimitrascu94@gmail.com>
2018-08-17 15:48:06 +0300

    Ex. 2.44 and picture processing

--- 2.2/2-2-4.rkt
+++ 2.2/2-2-4.rkt
@@ -1,3 +1,94 @@
 #lang sicp
+(#%require sicp-pict)
 
-(inc 43)
+
+; The wave painter.
+(define wave-segments
+ (list
+  (make-segment
+   (make-vect 0.006 0.840)
+   (make-vect 0.155 0.591))
+  (make-segment
+   (make-vect 0.006 0.635)
+   (make-vect 0.155 0.392))
+  (make-segment
+   (make-vect 0.304 0.646)
+   (make-vect 0.155 0.591))
+  (make-segment
+   (make-vect 0.298 0.591)
+   (make-vect 0.155 0.392))
+  (make-segment
+   (make-vect 0.304 0.646)
+   (make-vect 0.403 0.646))
+  (make-segment
+   (make-vect 0.298 0.591)
+   (make-vect 0.354 0.492))
+  (make-segment
+   (make-vect 0.403 0.646)
+   (make-vect 0.348 0.845))
+  (make-segment
+   (make-vect 0.354 0.492)
+   (make-vect 0.249 0.000))
+  (make-segment
+   (make-vect 0.403 0.000)
+   (make-vect 0.502 0.293))
+  (make-segment
+   (make-vect 0.502 0.293)
+   (make-vect 0.602 0.000))
+  (make-segment
+   (make-vect 0.348 0.845)
+   (make-vect 0.403 0.999))
+  (make-segment
+   (make-vect 0.602 0.999)
+   (make-vect 0.652 0.845))
+  (make-segment
+   (make-vect 0.652 0.845)
+   (make-vect 0.602 0.646))
+  (make-segment
+   (make-vect 0.602 0.646)
+   (make-vect 0.751 0.646))
+  (make-segment
+   (make-vect 0.751 0.646)
+   (make-vect 0.999 0.343))
+  (make-segment
+   (make-vect 0.751 0.000)
+   (make-vect 0.597 0.442))
+  (make-segment
+   (make-vect 0.597 0.442)
+   (make-vect 0.999 0.144))))
+
+(define dog (load-painter "dog.jpg"))
+(define wave (segments->painter wave-segments))
+
+(define (flipped-pairs painter)
+  (let ((painter2 (beside painter (flip-vert painter))))
+    (below painter2 painter2)))
+
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((small (right-split painter (- n 1))))
+        (beside painter (below small small)))))
+
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((small (up-split painter (- n 1))))
+        (below painter
+                (beside small small)))))
+
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (beside up up))
+              (bottom-right (below right right))
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
+(define (square-limit painter n)
+  (let ((quarter (corner-split painter n)))
+    (let ((half (beside (flip-horiz quarter) quarter)))
+      (below (flip-vert half) half))))
